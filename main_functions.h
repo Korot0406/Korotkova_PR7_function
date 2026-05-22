ef3c4000c92b09681a83cf15c2412ef4f933653d
Korot0406 <ik.0406@mail.ru>
2026-05-22 19:10:01 +0300

    Create task 2

--- main_functions.h
+++ main_functions.h
@@ -30,16 +30,34 @@ void process(char *str){
     cout << "Количество цифр в строке: " << k << "\n";
 }
 
-namespace namespace_string3
+namespace namespace_string1
 {
     void process(string &str){
-
+        int len = str.length();
+    
+        for(int i = 0; i < len; ++i){
+            if (str[i] >= '0' && str[i] <= '9'){
+                str[i] = '*';
+            }
+        }
     }
 }
 
 namespace namespace_string2
 {
-    void process(char *str){
+    void process(string &str){
+        int len = str.length();
+        string result = "";
+        int k = len;
 
+        for(int i = 0; i < len; ++i){
+            if (str[i] < '0' || str[i] > '9'){
+                result += str[i];
+                --k;
+            }
+        }
+        str = result;
+
+        cout << "Количество цифр в строке: " << k << "\n";
     }
 }
